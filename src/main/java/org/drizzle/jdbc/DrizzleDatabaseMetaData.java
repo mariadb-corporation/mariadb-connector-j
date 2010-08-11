56c1edc98b6f8d17dd42d987e310e854693bcb1a
Marcus Eriksson <marcuse@marcuse-laptop>
2010-08-11 11:30:08 +0200

    remove println

--- src/main/java/org/drizzle/jdbc/DrizzleDatabaseMetaData.java
+++ src/main/java/org/drizzle/jdbc/DrizzleDatabaseMetaData.java
@@ -1820,7 +1820,6 @@ public final class DrizzleDatabaseMetaData implements DatabaseMetaData {
             query += " AND table_schema = '" + schema + "'";
         }
         query += " ORDER BY column_name";
-        System.out.println(query);
         final Statement stmt = connection.createStatement();
         return stmt.executeQuery(query);
     }
