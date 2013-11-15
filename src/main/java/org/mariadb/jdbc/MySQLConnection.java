dfb864fb92dc4fd69247e25c54e7ef6a7453b0fe
Georg Richter <holzboote@googlemail.com>
2013-11-15 08:22:35 +0100

    Bug fix for CONJ-67: MySQLConnection class does not implement all JDBC4 methods and silently discards/ignores calls to them   Throw an exception for non supported JDBC 4.1 functions or return the default value (catalog=null, timeout=0)

--- src/main/java/org/mariadb/jdbc/MySQLConnection.java
+++ src/main/java/org/mariadb/jdbc/MySQLConnection.java
@@ -1299,27 +1299,28 @@ public final class MySQLConnection  implements Connection {
         return lowercaseTableNames;
     }
 	public void abort(Executor arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		
+      // TODO: not implemented yet
+      throw SQLExceptionMapper.getFeatureNotSupportedException("Not yet supported");
 	}
 
 	public int getNetworkTimeout() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+      // We don't support this yet, so we return the default value 0, which means
+      // no timeout is set. 
+      return 0;
 	}
 
 	public String getSchema() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		  // We support only catalog 
+		  return null;
 	}
 
 	public void setNetworkTimeout(Executor arg0, int arg1) throws SQLException {
-		// TODO Auto-generated method stub
-		
+		  // TODO: not implemented yet
+      throw SQLExceptionMapper.getFeatureNotSupportedException("Not yet supported");
 	}
 
 	public void setSchema(String arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		
+		  // We support only catalog 
+      throw SQLExceptionMapper.getFeatureNotSupportedException("Only catalogs are supported");
 	}
 }
