7a01fa8f0c34cbed7d902d54f43174da46c010f8
Massimo Siani <massimo.siani@skysql.com>
2014-06-18 10:45:06 -0400

    Add test case for CONJ-97

--- src/test/java/org/mariadb/jdbc/DriverTest.java
+++ src/test/java/org/mariadb/jdbc/DriverTest.java
@@ -1122,8 +1122,6 @@ public class DriverTest extends BaseTest{
             if(rs.getString("table_cat").equals("test_testdrop")) foundDb = true;
         }
         assertTrue(foundDb);
-
-
     }
 
      @Test
@@ -1610,5 +1608,18 @@ public class DriverTest extends BaseTest{
     public void emptyBatch() throws Exception {
         Statement st = connection.createStatement();
         st.executeBatch();
+    }
+    
+    @Test
+    public void createDbWithSpacesTest() throws SQLException {
+    	Connection conn;
+    	conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test with spaces?createDB=true&user=root");
+        DatabaseMetaData dbmd = conn.getMetaData();
+        ResultSet rs = dbmd.getCatalogs();
+        boolean foundDb = false;
+        while(rs.next()) {
+            if(rs.getString("table_cat").equals("test with spaces")) foundDb = true;
+        }
+        assertTrue(foundDb);
     }
 }
