258f26a040368c9e7643443bf56b63e18a766bfd
Massimo Siani <massimo.siani@skysql.com>
2014-07-09 08:57:29 -0400

    Fix for CONJ-80

--- src/test/java/org/mariadb/jdbc/DataSourceTest.java
+++ src/test/java/org/mariadb/jdbc/DataSourceTest.java
@@ -1,12 +1,13 @@
 package org.mariadb.jdbc;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertEquals;
+import javax.sql.DataSource;
+
+import org.junit.Test;
 
 public class DataSourceTest {
     @Test
@@ -28,5 +29,48 @@ public class DataSourceTest {
         }finally {
             connection.close();
         }
+    }
+    
+    /**
+     * CONJ-80
+     * @throws SQLException
+     */
+    @Test
+    public void setDatabaseNameTest() throws SQLException {
+    	MySQLDataSource ds = new MySQLDataSource("localhost", 3306, "test");
+    	Connection connection = ds.getConnection("root", "");
+    	connection.createStatement().execute("CREATE DATABASE IF NOT EXISTS test2");
+    	ds.setDatabaseName("test2");
+    	connection = ds.getConnection("root", "");
+    	assertEquals("test2", ds.getDatabaseName());
+    	assertEquals(ds.getDatabaseName(), connection.getCatalog());
+    	connection.createStatement().execute("DROP DATABASE IF EXISTS test2");
+    	connection.close();
+    }
+    
+    /**
+     * CONJ-80
+     * @throws SQLException
+     */
+    @Test
+    public void setServerNameTest() throws SQLException {
+    	MySQLDataSource ds = new MySQLDataSource("localhost", 3306, "test");
+    	Connection connection = ds.getConnection("root", "");
+    	ds.setServerName("127.0.0.1");
+    	connection = ds.getConnection("root", "");
+    	connection.close();
+    }
+    
+    /**
+     * CONJ-80
+     * @throws SQLException
+     */
+    @Test(expected=SQLException.class) // unless port 3307 can be used
+    public void setPortTest() throws SQLException {
+    	MySQLDataSource ds = new MySQLDataSource("localhost", 3306, "test");
+    	Connection connection = ds.getConnection("root", "");
+    	ds.setPort(3307);
+    	connection = ds.getConnection("root", "");
+    	connection.close();
     }
 }
