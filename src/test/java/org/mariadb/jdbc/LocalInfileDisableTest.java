cc7b8e2d17c334fa3311e16596d4da4512988564
kolzeq <diego.dupin@gmail.com>
2015-09-18 16:34:32 +0200

    [CONJ-22] adding test load data local file with prepareStatement

--- src/test/java/org/mariadb/jdbc/LocalInfileDisableTest.java
+++ src/test/java/org/mariadb/jdbc/LocalInfileDisableTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -24,6 +25,30 @@ public class LocalInfileDisableTest extends BaseTest {
         try {
             stmt = connection.createStatement();
             stmt.executeUpdate("LOAD DATA LOCAL INFILE 'dummy.tsv' INTO TABLE t (id, test)");
+        } catch (Exception e) {
+            ex = e;
+        } finally {
+            try {
+                stmt.close();
+            } catch (Exception ignore) {
+            }
+        }
+
+        Assert.assertNotNull("Expected an exception to be thrown", ex);
+        String message = ex.getMessage();
+        String expectedMessage = "Usage of LOCAL INFILE is disabled. To use it enable it via the connection property allowLocalInfile=true";
+        Assert.assertEquals(message, expectedMessage);
+    }
+
+
+    @Test
+    public void testPrepareLocalInfileWithoutInputStream() throws SQLException {
+        PreparedStatement stmt = null;
+        Exception ex = null;
+        try {
+            stmt = connection.prepareStatement("LOAD DATA LOCAL INFILE 'dummy.tsv' INTO TABLE t (id, test)");
+            //stmt.executeUpdate("LOAD DATA LOCAL INFILE 'dummy.tsv' INTO TABLE t (id, test)");
+            stmt.execute();
         } catch (Exception e) {
             ex = e;
         } finally {
