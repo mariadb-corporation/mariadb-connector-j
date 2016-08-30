79a21e520abc547df73510069621d341c01d6a6a
kolzeq <diego.dupin@gmail.com>
2016-08-31 01:41:49 +0200

    Release version 1.5.2

--- src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
+++ src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
@@ -51,10 +51,10 @@ OF SUCH DAMAGE.
 package org.mariadb.jdbc.internal.util.constant;
 
 public final class Version {
-    public static final String version = "1.5.2-SNAPSHOT";
+    public static final String version = "1.5.2";
     public static final int majorVersion = 1;
     public static final int minorVersion = 5;
     public static final int patchVersion = 2;
-    public static final String qualifier = "SNAPSHOT";
+    public static final String qualifier = "";
 
 }
