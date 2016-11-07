015a2aed490e1cabca9c8758714175c5e7cc11fb
Diego <diego.dupin@mariadb.com>
2016-11-07 23:35:17 +0100

    [misc] checkstyle correction + version tag

--- src/test/java/org/mariadb/jdbc/ExecuteBatchTest.java
+++ src/test/java/org/mariadb/jdbc/ExecuteBatchTest.java
@@ -9,11 +9,15 @@ import java.sql.*;
 
 public class ExecuteBatchTest extends BaseTest {
 
+    /**
+     * Create test tables.
+     *
+     * @throws SQLException if connection error occur
+     */
     @BeforeClass()
     public static void initClass() throws SQLException {
         createTable("ExecuteBatchTest", "id int not null primary key auto_increment, test varchar(100) , test2 int");
         createTable("ExecuteBatchUseBatchMultiSend", "test varchar(100)");
-
     }
 
     static String oneHundredLengthString = "";
