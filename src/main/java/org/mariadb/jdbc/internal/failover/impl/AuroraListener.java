a84d69d9fc783baed430fb4bc402b62f00bb367c
Diego <diego.dupin@mariadb.com>
2016-12-12 13:52:25 +0100

    [CONJ-395] Aurora read randomize correction

--- src/main/java/org/mariadb/jdbc/internal/failover/impl/AuroraListener.java
+++ src/main/java/org/mariadb/jdbc/internal/failover/impl/AuroraListener.java
@@ -249,6 +249,9 @@ public class AuroraListener extends MastersSlavesListener {
                     endpoints.add(resultSet.getString(1) + urlEndStr);
                 }
 
+                //randomize order for distributed load-balancing
+                Collections.shuffle(endpoints);
+
             } finally {
                 proxy.lock.unlock();
             }
