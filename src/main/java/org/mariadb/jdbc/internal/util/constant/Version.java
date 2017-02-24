10370a28308cfc416014384dba744295b8c0d75d
Diego <diego.dupin@mariadb.com>
2017-02-24 18:00:30 +0100

    [misc] Bump 1.5.9-SNAPSHOT version

--- src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
+++ src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
@@ -51,10 +51,10 @@ OF SUCH DAMAGE.
 package org.mariadb.jdbc.internal.util.constant;
 
 public final class Version {
-    public static final String version = "1.5.8";
+    public static final String version = "1.5.9-SNAPSHOT";
     public static final int majorVersion = 1;
     public static final int minorVersion = 5;
-    public static final int patchVersion = 8;
-    public static final String qualifier = "";
+    public static final int patchVersion = 9;
+    public static final String qualifier = "SNAPSHOT";
 
 }
