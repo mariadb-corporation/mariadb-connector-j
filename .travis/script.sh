8ae4d8f32032df16bb11722d1212f930fa264d4d
Diego <diego.dupin@mariadb.com>
2017-05-07 21:38:18 +0200

    [misc] stopping maxscale for travis tests

--- .travis/script.sh
+++ .travis/script.sh
@@ -40,6 +40,12 @@ mvn clean test $urlString -DtestSingleHost=$testSingleHost $ADDITIONNAL_VARIABLE
     -Dkeystore2Path="/etc/mysql/fullclient-keystore.jks" -Dkeystore2Password="kspass" -DkeyPassword="kspasskey"  \
     -Dkeystore2PathP12="/etc/mysql/fullclient-keystore.p12"
 
+if [ "$TYPE" == "MAXSCALE" ]
+then
+sudo service maxscale stop
+fi
+
+
 if [ ! -n "$AURORA" ]
 then
     sudo service mysql stop
