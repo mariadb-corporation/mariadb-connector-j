496da260c0f04ca7753a8a14bf2eac4c1bbdb9e2
Diego <diego.dupin@mariadb.com>
2017-06-09 15:42:18 +0200

    [misc] ensure stability if option "enablePacketDebug" is set and IOException occur in when establishing socket

--- src/main/java/org/mariadb/jdbc/internal/protocol/AbstractConnectProtocol.java
+++ src/main/java/org/mariadb/jdbc/internal/protocol/AbstractConnectProtocol.java
@@ -1334,7 +1334,7 @@ public abstract class AbstractConnectProtocol implements Protocol {
     }
 
     public String getTraces() {
-        if (options.enablePacketDebug) return traceCache.printStack();
+        if (options.enablePacketDebug && traceCache != null) return traceCache.printStack();
         return "";
     }
 }
