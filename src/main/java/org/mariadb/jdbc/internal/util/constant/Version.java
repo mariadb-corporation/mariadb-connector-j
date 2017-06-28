5ee7e6fc43729375e7c9a2e9516a4f74c8ed1a6e
rusher <diego.dupin@gmail.com>
2017-06-28 09:43:48 +0200

    bump 1.6.3-SNAPSHOT version

--- src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
+++ src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
@@ -53,9 +53,9 @@
 package org.mariadb.jdbc.internal.util.constant;
 
 public final class Version {
-    public static final String version = "1.6.2-SNAPSHOT";
+    public static final String version = "1.6.3-SNAPSHOT";
     public static final int majorVersion = 1;
     public static final int minorVersion = 6;
-    public static final int patchVersion = 2;
+    public static final int patchVersion = 3;
     public static final String qualifier = "-SNAPSHOT";
 }
