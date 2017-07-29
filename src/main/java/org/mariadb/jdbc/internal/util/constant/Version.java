74e851f84a4923a8d48345e341486841ba2ba7ca
rusher <diego.dupin@gmail.com>
2017-07-29 12:04:38 +0200

    bump 2.1.0 version

--- src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
+++ src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
@@ -53,9 +53,9 @@
 package org.mariadb.jdbc.internal.util.constant;
 
 public final class Version {
-    public static final String version = "2.1.0-SNAPSHOT";
+    public static final String version = "2.1.0";
     public static final int majorVersion = 2;
     public static final int minorVersion = 1;
     public static final int patchVersion = 0;
-    public static final String qualifier = "-SNAPSHOT";
+    public static final String qualifier = "";
 }
