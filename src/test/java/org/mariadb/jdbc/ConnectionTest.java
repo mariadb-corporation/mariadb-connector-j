d9a0cc948f2528a09fa51bb56b9f8784aafadf3d
rusher <diego.dupin@gmail.com>
2017-12-05 15:00:24 +0100

    [CONJ-501] support for ed25519 authentication plugin

--- src/test/java/org/mariadb/jdbc/ConnectionTest.java
+++ src/test/java/org/mariadb/jdbc/ConnectionTest.java
@@ -510,4 +510,25 @@ public class ConnectionTest extends BaseTest {
         }
     }
 
+    @Test
+    public void verificationEd25519AuthPlugin() throws Throwable {
+        Assume.assumeTrue(isMariadbServer() && minVersion(10, 2));
+        Statement stmt = sharedConnection.createStatement();
+        stmt.execute("INSTALL SONAME 'auth_ed25519'");
+        stmt.execute("CREATE USER verificationEd25519AuthPlugin@'%' IDENTIFIED "
+                + "VIA ed25519 USING 'ZIgUREUg5PVgQ6LskhXmO+eZLS0nC8be6HPjYWR4YJY'");
+        stmt.execute("CREATE USER verificationEd25519AuthPlugin@'localhost' IDENTIFIED "
+                + "VIA ed25519 USING 'ZIgUREUg5PVgQ6LskhXmO+eZLS0nC8be6HPjYWR4YJY'");
+        stmt.execute("GRANT ALL on " + database + ".* to verificationEd25519AuthPlugin@'%'");
+        stmt.execute("GRANT ALL on " + database + ".* to verificationEd25519AuthPlugin@'localhost'");
+
+        String url = "jdbc:mariadb://" + hostname + ((port == 0) ? "" : ":" + port) + "/" + database
+                + "?user=verificationEd25519AuthPlugin&password=secret";
+
+        try (Connection connection = openNewConnection(url)) {
+            //must have succeed
+        }
+        stmt.execute("drop user verificationEd25519AuthPlugin@'%'");
+        stmt.execute("drop user verificationEd25519AuthPlugin@'localhost'");
+    }
 }
