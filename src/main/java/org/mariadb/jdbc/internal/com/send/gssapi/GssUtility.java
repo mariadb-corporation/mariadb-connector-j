fb81de3548537e8777b79e563eb13562602afd5f
rusher <diego.dupin@mariadb.com>
2018-09-03 02:27:23 -0700

    [CONJ-634] avoid import of non dependency to permit compilation Ahead of time

---
+++ src/main/java/org/mariadb/jdbc/internal/com/send/gssapi/GssUtility.java
@@ -0,0 +1,33 @@
+package org.mariadb.jdbc.internal.com.send.gssapi;
+
+import com.sun.jna.Platform;
+import org.mariadb.jdbc.internal.io.input.PacketInputStream;
+
+import java.util.function.BiFunction;
+
+public class GssUtility {
+
+    /**
+     * Get authentication method according to classpath.
+     * Windows native authentication is using Waffle-jna.
+     *
+     * @return authentication method
+     */
+    public static BiFunction<PacketInputStream, Integer, GssapiAuth> getAuthenticationMethod() {
+        try {
+            //Waffle-jna has jna as dependency, so if not available on classpath, just use standard authentication
+            if (Platform.isWindows()) {
+                try {
+                    Class.forName("waffle.windows.auth.impl.WindowsAuthProviderImpl");
+                    return (reader, packSeq) -> new WindowsNativeSspiAuthentication(reader, packSeq);
+                } catch (ClassNotFoundException cle) {
+                    //waffle not in the classpath
+                }
+            }
+        } catch (Throwable cle) {
+            //jna jar's are not in classpath
+        }
+        return (reader, packSeq) -> new StandardGssapiAuthentication(reader, packSeq);
+    }
+
+}
---
+++ src/main/java/org/mariadb/jdbc/internal/io/socket/SocketUtility.java
@@ -0,0 +1,41 @@
+package org.mariadb.jdbc.internal.io.socket;
+
+import com.sun.jna.Platform;
+import org.mariadb.jdbc.internal.util.Utils;
+
+import java.io.IOException;
+
+public class SocketUtility {
+
+    @SuppressWarnings("unchecked")
+    public static SocketHandlerFunction getSocketHandler() {
+        try {
+            //forcing use of JNA to ensure AOT compilation
+            Platform.getOSType();
+
+            return (urlParser, host) -> {
+                if (urlParser.getOptions().pipe != null) {
+                    return new NamedPipeSocket(host, urlParser.getOptions().pipe);
+                } else if (urlParser.getOptions().localSocket != null) {
+                    try {
+                        return new UnixDomainSocket(urlParser.getOptions().localSocket);
+                    } catch (RuntimeException re) {
+                        throw new IOException(re.getMessage(), re.getCause());
+                    }
+                } else if (urlParser.getOptions().sharedMemory != null) {
+                    try {
+                        return new SharedMemorySocket(urlParser.getOptions().sharedMemory);
+                    } catch (RuntimeException re) {
+                        throw new IOException(re.getMessage(), re.getCause());
+                    }
+                } else {
+                    return Utils.standardSocket(urlParser, host);
+                }
+
+            };
+        } catch (Throwable cle) {
+            //jna jar's are not in classpath
+        }
+        return (urlParser, host) -> Utils.standardSocket(urlParser, host);
+    }
+}
