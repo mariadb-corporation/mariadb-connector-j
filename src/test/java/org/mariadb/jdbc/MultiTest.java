8b2f79cf1bf77684d924018f51807d8bd3be5c49
rusher <diego.dupin@gmail.com>
2019-06-17 14:42:12 +0200

    [misc] correction test for multi-insert value

--- src/test/java/org/mariadb/jdbc/MultiTest.java
+++ src/test/java/org/mariadb/jdbc/MultiTest.java
@@ -1452,7 +1452,8 @@ public class MultiTest extends BaseTest {
         try (ResultSet rs = pstmt.getGeneratedKeys()) {
           assertTrue(rs.next());
           assertEquals(1, rs.getInt(1));
-          assertFalse(rs.next());
+          //Will change in 2.5.0
+          assertTrue(rs.next());
         }
       }
     }
