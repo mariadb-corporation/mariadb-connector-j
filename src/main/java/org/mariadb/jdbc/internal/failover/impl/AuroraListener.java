5c22012461f45fd9a8634f4564478353c3e5fd7c
Tim Roerig <troerig@grubhub.com>
2019-08-23 15:08:46 -0500

    Modified reg-ex patterns to match Aurora's read-only endpoints

--- src/main/java/org/mariadb/jdbc/internal/failover/impl/AuroraListener.java
+++ src/main/java/org/mariadb/jdbc/internal/failover/impl/AuroraListener.java
@@ -76,7 +76,7 @@ public class AuroraListener extends MastersSlavesListener {
   private static final Logger logger = Logger.getLogger(AuroraListener.class.getName());
   private final Pattern auroraDnsPattern =
       Pattern.compile(
-          "(.+)\\.(cluster-)?([a-zA-Z0-9]+\\.[a-zA-Z0-9\\-]+\\.rds\\.amazonaws\\.com)",
+          "(.+)\\.(cluster-|cluster-ro-)?([a-zA-Z0-9]+\\.[a-zA-Z0-9\\-]+\\.rds\\.amazonaws\\.com)",
           Pattern.CASE_INSENSITIVE);
   private final HostAddress clusterHostAddress;
   private String clusterDnsSuffix = null;
