add4a515c33345d83ff54a0a5b822009cfe75611
Tim Roerig <troerig@grubhub.com>
2019-11-19 12:22:47 -0600

    Added unit test to help illustrate the problem

--- src/main/java/org/mariadb/jdbc/internal/failover/impl/AuroraListener.java
+++ src/main/java/org/mariadb/jdbc/internal/failover/impl/AuroraListener.java
@@ -130,6 +130,10 @@ public class AuroraListener extends MastersSlavesListener {
     return null;
   }
 
+  public String getClusterDnsSuffix() {
+    return clusterDnsSuffix;
+  }
+
   public HostAddress getClusterHostAddress() {
     return clusterHostAddress;
   }
