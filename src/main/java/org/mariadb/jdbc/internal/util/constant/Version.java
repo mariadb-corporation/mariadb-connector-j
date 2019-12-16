e56a115ff5da86ac717cb03262d60ac7c8b44e92
rusher <diego.dupin@gmail.com>
2019-12-16 10:42:06 +0100

    [misc] changing travis link to https://travis-ci.org/mariadb-corporation/

--- src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
+++ src/main/java/org/mariadb/jdbc/internal/util/constant/Version.java
@@ -53,9 +53,9 @@
 package org.mariadb.jdbc.internal.util.constant;
 
 public final class Version {
-  public static final String version = "2.5.2";
+  public static final String version = "2.5.3-SNAPSHOT";
   public static final int majorVersion = 2;
   public static final int minorVersion = 5;
-  public static final int patchVersion = 2;
-  public static final String qualifier = "";
+  public static final int patchVersion = 3;
+  public static final String qualifier = "-SNAPSHOT";
 }
