3029757c7f175eccf5216b4ed27fb0a79f7fda7a
rusher <diego.dupin@gmail.com>
2020-03-06 10:59:37 +0100

    [CONJ-767] permit using Aurora RO endpoint - code style correction after merge

--- src/test/java/org/mariadb/jdbc/AuroraListenerTest.java
+++ src/test/java/org/mariadb/jdbc/AuroraListenerTest.java
@@ -1,32 +1,36 @@
 package org.mariadb.jdbc;
 
+import static org.junit.Assert.assertEquals;
+
+import java.sql.SQLException;
 import org.junit.Test;
 import org.mariadb.jdbc.internal.failover.impl.AuroraListener;
 import org.mariadb.jdbc.internal.util.pool.GlobalStateInfo;
 
-import java.sql.SQLException;
-
-import static org.junit.Assert.assertEquals;
-
 public class AuroraListenerTest {
 
-    public static final String EXPECTED_CLUSTER_DNS_SUFFIX = "ZZZZZZZZZZZZ.us-east-1.rds.amazonaws.com";
+  public static final String EXPECTED_CLUSTER_DNS_SUFFIX =
+      "ZZZZZZZZZZZZ.us-east-1.rds.amazonaws.com";
 
-    @Test
-    public void testWithWriterCluster() throws SQLException {
-        GlobalStateInfo globalInfo = new GlobalStateInfo();
-        UrlParser urlParser = UrlParser.parse("jdbc:mysql:aurora://XXXX.cluster-ZZZZZZZZZZZZ.us-east-1.rds.amazonaws.com:3306/XXXX");
-        AuroraListener listener = new AuroraListener(urlParser, globalInfo);
+  @Test
+  public void testWithWriterCluster() throws SQLException {
+    GlobalStateInfo globalInfo = new GlobalStateInfo();
+    UrlParser urlParser =
+        UrlParser.parse(
+            "jdbc:mysql:aurora://XXXX.cluster-ZZZZZZZZZZZZ.us-east-1.rds.amazonaws.com:3306/XXXX");
+    AuroraListener listener = new AuroraListener(urlParser, globalInfo);
 
-        assertEquals(listener.getClusterDnsSuffix(), EXPECTED_CLUSTER_DNS_SUFFIX);
-    }
+    assertEquals(listener.getClusterDnsSuffix(), EXPECTED_CLUSTER_DNS_SUFFIX);
+  }
 
-    @Test
-    public void testWithReaderCluster() throws SQLException {
-        GlobalStateInfo globalInfo = new GlobalStateInfo();
-        UrlParser urlParser = UrlParser.parse("jdbc:mysql:aurora://XXXX.cluster-ro-ZZZZZZZZZZZZ.us-east-1.rds.amazonaws.com:3306/XXXX");
-        AuroraListener listener = new AuroraListener(urlParser, globalInfo);
+  @Test
+  public void testWithReaderCluster() throws SQLException {
+    GlobalStateInfo globalInfo = new GlobalStateInfo();
+    UrlParser urlParser =
+        UrlParser.parse(
+            "jdbc:mysql:aurora://XXXX.cluster-ro-ZZZZZZZZZZZZ.us-east-1.rds.amazonaws.com:3306/XXXX");
+    AuroraListener listener = new AuroraListener(urlParser, globalInfo);
 
-        assertEquals(listener.getClusterDnsSuffix(), EXPECTED_CLUSTER_DNS_SUFFIX);
-    }
+    assertEquals(listener.getClusterDnsSuffix(), EXPECTED_CLUSTER_DNS_SUFFIX);
+  }
 }
