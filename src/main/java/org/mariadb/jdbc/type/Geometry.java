3e8a7dd87e95d595edd733995736a4e30203a908
kolzeq <diego.dupin@gmail.com>
2021-04-13 10:35:25 +0200

    misc - bump dependencies

--- src/main/java/org/mariadb/jdbc/type/Geometry.java
+++ src/main/java/org/mariadb/jdbc/type/Geometry.java
@@ -14,80 +14,78 @@ public interface Geometry {
 
   static Geometry getGeometry(ReadableByteBuf buf, int length, ColumnDefinitionPacket column)
       throws SQLDataException {
-    if (length > 0) {
-      boolean littleEndian = buf.readByte() == 0x01;
-      int dataType = littleEndian ? buf.readInt() : buf.readIntBE();
-      switch (dataType) {
-        case 1:
-          // wkbPoint
-          return parsePoint(littleEndian, buf);
+    if (length == 0) return null;
+    boolean littleEndian = buf.readByte() == 0x01;
+    int dataType = littleEndian ? buf.readInt() : buf.readIntBE();
+    switch (dataType) {
+      case 1:
+        // wkbPoint
+        return parsePoint(littleEndian, buf);
 
-        case 2:
-          // wkbLineString
-          int pointNumber = littleEndian ? buf.readInt() : buf.readIntBE();
-          Point[] points = new Point[pointNumber];
-          for (int i = 0; i < pointNumber; i++) {
-            points[i] = parsePoint(littleEndian, buf);
-          }
-          return new LineString(points, true);
+      case 2:
+        // wkbLineString
+        int pointNumber = littleEndian ? buf.readInt() : buf.readIntBE();
+        Point[] points = new Point[pointNumber];
+        for (int i = 0; i < pointNumber; i++) {
+          points[i] = parsePoint(littleEndian, buf);
+        }
+        return new LineString(points, true);
 
-        case 3:
-          // wkbPolygon
-          int numRings = littleEndian ? buf.readInt() : buf.readIntBE();
-          LineString[] lines = new LineString[numRings];
-          for (int i = 0; i < numRings; i++) {
-            int pointNb = littleEndian ? buf.readInt() : buf.readIntBE();
-            Point[] lsPoints = new Point[pointNb];
-            for (int j = 0; j < pointNb; j++) {
-              lsPoints[j] = parsePoint(littleEndian, buf);
-            }
-            lines[i] = new LineString(lsPoints, false);
-          }
-          return new Polygon(lines);
-
-        case 4:
-          // wkbMultiPoint
+      case 3:
+        // wkbPolygon
+        int numRings = littleEndian ? buf.readInt() : buf.readIntBE();
+        LineString[] lines = new LineString[numRings];
+        for (int i = 0; i < numRings; i++) {
           int pointNb = littleEndian ? buf.readInt() : buf.readIntBE();
-          Point[] pointArr = new Point[pointNb];
-          for (int i = 0; i < pointNb; i++) {
-            pointArr[i] = (Point) getGeometry(buf, length, column);
+          Point[] lsPoints = new Point[pointNb];
+          for (int j = 0; j < pointNb; j++) {
+            lsPoints[j] = parsePoint(littleEndian, buf);
           }
-          return new MultiPoint(pointArr);
+          lines[i] = new LineString(lsPoints, false);
+        }
+        return new Polygon(lines);
 
-        case 5:
-          // wkbMultiLinestring
-          int multiNb = littleEndian ? buf.readInt() : buf.readIntBE();
-          LineString[] multiLines = new LineString[multiNb];
-          for (int i = 0; i < multiNb; i++) {
-            multiLines[i] = (LineString) getGeometry(buf, length, column);
-          }
-          return new MultiLineString(multiLines);
+      case 4:
+        // wkbMultiPoint
+        int pointNb = littleEndian ? buf.readInt() : buf.readIntBE();
+        Point[] pointArr = new Point[pointNb];
+        for (int i = 0; i < pointNb; i++) {
+          pointArr[i] = (Point) getGeometry(buf, length, column);
+        }
+        return new MultiPoint(pointArr);
 
-        case 6:
-          // wkbMultiPolygon
-          int multiPolyNb = littleEndian ? buf.readInt() : buf.readIntBE();
-          Polygon[] multiPolygons = new Polygon[multiPolyNb];
-          for (int i = 0; i < multiPolyNb; i++) {
-            multiPolygons[i] = (Polygon) getGeometry(buf, length, column);
-          }
-          return new MultiPolygon(multiPolygons);
+      case 5:
+        // wkbMultiLinestring
+        int multiNb = littleEndian ? buf.readInt() : buf.readIntBE();
+        LineString[] multiLines = new LineString[multiNb];
+        for (int i = 0; i < multiNb; i++) {
+          multiLines[i] = (LineString) getGeometry(buf, length, column);
+        }
+        return new MultiLineString(multiLines);
 
-        case 7:
-          // wkbGeometryCollection
-          int multiCollNb = littleEndian ? buf.readInt() : buf.readIntBE();
-          Geometry[] multiGeos = new Geometry[multiCollNb];
-          for (int i = 0; i < multiCollNb; i++) {
-            multiGeos[i] = getGeometry(buf, length, column);
-          }
-          return new GeometryCollection(multiGeos);
+      case 6:
+        // wkbMultiPolygon
+        int multiPolyNb = littleEndian ? buf.readInt() : buf.readIntBE();
+        Polygon[] multiPolygons = new Polygon[multiPolyNb];
+        for (int i = 0; i < multiPolyNb; i++) {
+          multiPolygons[i] = (Polygon) getGeometry(buf, length, column);
+        }
+        return new MultiPolygon(multiPolygons);
 
-        default:
-          // ERROR
-          buf.skip(length - 5);
-          throw new SQLDataException(
-              String.format("Data type %s cannot be decoded as Geometry", column.getType()));
-      }
+      case 7:
+        // wkbGeometryCollection
+        int multiCollNb = littleEndian ? buf.readInt() : buf.readIntBE();
+        Geometry[] multiGeos = new Geometry[multiCollNb];
+        for (int i = 0; i < multiCollNb; i++) {
+          multiGeos[i] = getGeometry(buf, length, column);
+        }
+        return new GeometryCollection(multiGeos);
+
+      default:
+        // ERROR
+        buf.skip(length - 5);
+        throw new SQLDataException(
+            String.format("Data type %s cannot be decoded as Geometry", column.getType()));
     }
-    return null;
   }
 }
--- src/test/java/org/mariadb/jdbc/unit/type/GeometryTest.java
+++ src/test/java/org/mariadb/jdbc/unit/type/GeometryTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import org.mariadb.jdbc.client.ReadableByteBuf;
+import org.mariadb.jdbc.codec.DataType;
+import org.mariadb.jdbc.message.server.ColumnDefinitionPacket;
 import org.mariadb.jdbc.type.*;
 import org.mariadb.jdbc.util.MutableInt;
 
@@ -167,5 +169,137 @@ public class GeometryTest {
     assertNotEquals("wrong", geo);
     assertNotEquals(
         new MultiPoint(new Point[] {new Point(0, 0), new Point(0, 11), new Point(10, 0)}), geo);
+  }
+
+  @Test
+  public void testMultiLinestringEncoding() throws SQLException {
+    byte[] ptBytes =
+        hexStringToByteArray(
+            "00"
+                + "00 00 00 05"
+                + "00 00 00 01 "
+                + "00 "
+                + "00000002"
+                + "00000003"
+                + "00 00 00 00 00 00 00 00"
+                + "00 00 00 00 00 00 00 00"
+                + "00 00 00 00 00 00 00 00"
+                + "40 24 00 00 00 00 00 00"
+                + "40 24 00 00 00 00 00 00"
+                + "00 00 00 00 00 00 00 00");
+    ReadableByteBuf readBuf = new ReadableByteBuf(new MutableInt(), ptBytes, ptBytes.length);
+    Geometry geo = Geometry.getGeometry(readBuf, ptBytes.length, null);
+    assertEquals("MULTILINESTRING((0.0 0.0,0.0 10.0,10.0 0.0))", geo.toString());
+    assertEquals(geo, geo);
+    MultiLineString ml =
+        new MultiLineString(
+            new LineString[] {
+              new LineString(
+                  new Point[] {new Point(0, 0), new Point(0, 10), new Point(10, 0)}, true)
+            });
+    assertEquals(ml, geo);
+    assertEquals(ml.hashCode(), geo.hashCode());
+    assertFalse(geo.equals("wrong"));
+  }
+
+  @Test
+  public void testMultiPolygonEncoding() throws SQLException {
+    byte[] ptBytes =
+        hexStringToByteArray(
+            "00"
+                + "00 00 00 06"
+                + "00 00 00 01 "
+                + "00 "
+                + "00 00 00 03  "
+                + "00 00 00 01  "
+                + "00 00 00 07  "
+                + "3F F0 00 00 00 00 00 00  "
+                + "3F F0 00 00 00 00 00 00  "
+                + "3F F0 00 00 00 00 00 00  "
+                + "40 14 00 00 00 00 00 00  "
+                + "40 10 00 00 00 00 00 00  "
+                + "40 22 00 00 00 00 00 00  "
+                + "40 18 00 00 00 00 00 00  "
+                + "40 22 00 00 00 00 00 00  "
+                + "40 22 00 00 00 00 00 00  "
+                + "40 08 00 00 00 00 00 00  "
+                + "40 1C 00 00 00 00 00 00  "
+                + "40 00 00 00 00 00 00 00  "
+                + "3F F0 00 00 00 00 00 00  "
+                + "3F F0 00 00 00 00 00 00");
+    ReadableByteBuf readBuf = new ReadableByteBuf(new MutableInt(), ptBytes, ptBytes.length);
+    Geometry geo = Geometry.getGeometry(readBuf, ptBytes.length, null);
+    assertEquals(
+        "MULTIPOLYGON(((1.0 1.0,1.0 5.0,4.0 9.0,6.0 9.0,9.0 3.0,7.0 2.0,1.0 1.0)))",
+        geo.toString());
+    assertEquals(geo, geo);
+    MultiPolygon ml =
+        new MultiPolygon(
+            new Polygon[] {
+              new Polygon(
+                  new LineString[] {
+                    new LineString(
+                        new Point[] {
+                          new Point(1, 1),
+                          new Point(1, 5),
+                          new Point(4, 9),
+                          new Point(6, 9),
+                          new Point(9, 3),
+                          new Point(7, 2),
+                          new Point(1, 1)
+                        },
+                        false)
+                  })
+            });
+    assertEquals(ml, geo);
+    assertEquals(ml.hashCode(), geo.hashCode());
+    assertFalse(geo.equals("wrong"));
+  }
+
+  @Test
+  public void testMultiGeoEncoding() throws SQLException {
+    byte[] ptBytes =
+        hexStringToByteArray(
+            "00"
+                + "00 00 00 07"
+                + "00 00 00 02 "
+                + "00 "
+                + "00 00 00 01"
+                + "40 00 00 00 00 00 00 00 "
+                + "40 10 00 00 00 00 00 00"
+                + "00"
+                + "00000002"
+                + "00000003"
+                + "0000000000000000"
+                + "0000000000000000"
+                + "00 00 00 00 00 00 00 00"
+                + "40 24 00 00 00 00 00 00"
+                + "40 24 00 00 00 00 00 00"
+                + "00 00 00 00 00 00 00 00");
+    ReadableByteBuf readBuf = new ReadableByteBuf(new MutableInt(), ptBytes, ptBytes.length);
+    Geometry geo = Geometry.getGeometry(readBuf, ptBytes.length, null);
+    assertEquals(
+        "GEOMETRYCOLLECTION(POINT(2.0 4.0),LINESTRING(0.0 0.0,0.0 10.0,10.0 0.0))", geo.toString());
+    GeometryCollection geo1 =
+        new GeometryCollection(
+            new Geometry[] {
+              new Point(2, 4),
+              new LineString(
+                  new Point[] {new Point(0, 0), new Point(0, 10), new Point(10, 0)}, true)
+            });
+    assertEquals(geo1, geo);
+  }
+
+  @Test
+  public void testWrongEncoding() throws SQLException {
+    byte[] ptBytes = hexStringToByteArray("00 00 00 00 08");
+    ReadableByteBuf readBuf = new ReadableByteBuf(new MutableInt(), ptBytes, ptBytes.length);
+    assertThrows(
+        SQLException.class,
+        () ->
+            Geometry.getGeometry(
+                readBuf, ptBytes.length, ColumnDefinitionPacket.create("test", DataType.GEOMETRY)));
+    assertNull(
+        Geometry.getGeometry(new ReadableByteBuf(new MutableInt(), new byte[0], 0), 0, null));
   }
 }
