54ac944ac4478f8efdb9d130c1f1e5f5fcc41e91
diego Dupin <diego.dupinmariadb.com>
2021-10-21 14:17:20 +0200

    [misc] benchmark stable multi columns results

--- src/benchmark/README.md
+++ src/benchmark/README.md
@@ -8,13 +8,13 @@
 
 How to run : 
 ```script
-mvn clean package -P bench -Dmaven.test.skip
+mvn clean package -P bench -DskipTests
 
 # run all benchmarks
 java -Duser.country=US -Duser.language=en -jar target/benchmarks.jar
 
 # run a specific benchmark
-java -Duser.country=US -Duser.language=en -jar target/benchmarks.jar "Select_1_user"
+java -Duser.country=US -Duser.language=en -jar target/benchmarks.jar "Select_100_cols"
 ```
 
 Configuration by system properties :
---
+++ src/benchmark/java/org/mariadb/jdbc/Select_100_cols.java
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (c) 2012-2014 Monty Program Ab
+// Copyright (c) 2015-2021 MariaDB Corporation Ab
+
+package org.mariadb.jdbc;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class Select_100_cols extends Common {
+
+  @Benchmark
+  public int[] text(MyState state) throws Throwable {
+    return run(state.connectionText);
+  }
+
+  @Benchmark
+  public int[] binary(MyState state) throws Throwable {
+    return run(state.connectionBinary);
+  }
+
+  private int[] run(Connection con) throws Throwable {
+
+    try (PreparedStatement prep = con.prepareStatement("select * FROM test100")) {
+      ResultSet rs = prep.executeQuery();
+      rs.next();
+      int[] objs = new int[100];
+      for (int i = 0; i < 100; i++) {
+        objs[i] = rs.getInt(i + 1);
+      }
+      return objs;
+    }
+  }
+
+}
