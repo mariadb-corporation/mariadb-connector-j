baae3db3fdb7fd319e370be558ccf3c521d68cf3
diego Dupin <diego.dupinmariadb.com>
2021-11-15 14:34:20 +0100

    [misc] setting "transaction read only" only for replica

--- src/main/java/org/mariadb/jdbc/client/impl/StandardClient.java
+++ src/main/java/org/mariadb/jdbc/client/impl/StandardClient.java
@@ -295,10 +295,9 @@ public class StandardClient implements Client, AutoCloseable {
       commands.add("show status like 'wsrep_local_state'");
     }
 
-    if (context.getVersion().versionGreaterOrEqual(5, 6, 5)) {
-      commands.add(
-          "SET SESSION TRANSACTION "
-              + ((hostAddress != null && !hostAddress.primary) ? "READ ONLY" : "READ WRITE"));
+    if (!hostAddress.primary
+            && context.getVersion().versionGreaterOrEqual(5, 6, 5)) {
+      commands.add("SET SESSION TRANSACTION READ ONLY");
     }
 
     try {
