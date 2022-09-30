df715bd513ac774dd8ccc61b3221a671b25e832f
diego <diego.dupin@gmail.com>
2022-09-30 16:10:32 +0200

    [misc] ensure test stability for galera

--- src/test/java/org/mariadb/jdbc/DistributedTransactionTest.java
+++ src/test/java/org/mariadb/jdbc/DistributedTransactionTest.java
@@ -73,7 +73,9 @@ public class DistributedTransactionTest extends BaseTest {
   @BeforeClass()
   public static void initClass() throws SQLException {
     Assume.assumeTrue(
-        !"skysql".equals(System.getenv("srv")) && !"skysql-ha".equals(System.getenv("srv")));
+        !"skysql".equals(System.getenv("srv"))
+            && !"skysql-ha".equals(System.getenv("srv"))
+            && !"galera".equals(System.getenv("srv")));
     drop();
     try (Statement stmt = sharedConnection.createStatement()) {
       stmt.execute("CREATE TABLE xatable(i int)");
