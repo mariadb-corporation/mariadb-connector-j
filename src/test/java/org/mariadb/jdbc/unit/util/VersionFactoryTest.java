2756ad35836577b2e3687390c98881cce902c21c
diego <diego.dupin@gmail.com>
2023-01-19 11:43:14 +0100

    [misc] test: use mysql 8 using native password by default

--- src/test/java/org/mariadb/jdbc/unit/util/VersionFactoryTest.java
+++ src/test/java/org/mariadb/jdbc/unit/util/VersionFactoryTest.java
@@ -1,18 +1,17 @@
 package org.mariadb.jdbc.unit.util;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.mariadb.jdbc.util.Version;
 import org.mariadb.jdbc.util.VersionFactory;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 class VersionFactoryTest {
 
-    @Test
-    public void testGetInstance() {
-        Version actual = VersionFactory.getInstance();
+  @Test
+  public void testGetInstance() {
+    Version actual = VersionFactory.getInstance();
 
-        assertNotNull(actual);
-    }
-
+    assertNotNull(actual);
+  }
 }
