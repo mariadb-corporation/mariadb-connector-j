c3380a069d81c59f17777573cce9cbf0a2fce495
rusher <diego.dupin@gmail.com>
2023-06-19 18:20:41 +0200

    [misc] update copyright year

--- src/main/java/org/mariadb/jdbc/client/result/rowdecoder/BinaryRowDecoder.java
+++ src/main/java/org/mariadb/jdbc/client/result/rowdecoder/BinaryRowDecoder.java
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (c) 2012-2014 Monty Program Ab
+// Copyright (c) 2015-2023 MariaDB Corporation Ab
+
 package org.mariadb.jdbc.client.result.rowdecoder;
 
 import static org.mariadb.jdbc.client.result.Result.NULL_LENGTH;
