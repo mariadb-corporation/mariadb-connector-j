42115581b4471fc2c20ee134832dd93c71bfbe37
rusher <diego.dupin@gmail.com>
2024-02-08 11:02:21 +0100

    update copyright year

--- src/main/java/org/mariadb/jdbc/client/result/ResultSetMetaData.java
+++ src/main/java/org/mariadb/jdbc/client/result/ResultSetMetaData.java
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 // Copyright (c) 2012-2014 Monty Program Ab
-// Copyright (c) 2015-2023 MariaDB Corporation Ab
+// Copyright (c) 2015-2024 MariaDB Corporation Ab
 package org.mariadb.jdbc.client.result;
 
 import java.sql.SQLException;
