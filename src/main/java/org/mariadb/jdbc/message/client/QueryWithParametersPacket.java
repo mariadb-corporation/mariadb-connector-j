2669885edcd6448252281f6ef30b1d6b7639a9d8
Greg Knox <knoxg@randomnoun.com>
2025-02-22 17:59:47 +1000

    CONJ-1238: QueryWithParametersPacket can now contain more than one set of parameters; encode() will rewrite SQL to supply multiple parameters if possible and rewriteBatchStatements enabled

--- src/main/java/org/mariadb/jdbc/message/client/QueryWithParametersPacket.java
+++ src/main/java/org/mariadb/jdbc/message/client/QueryWithParametersPacket.java
@@ -6,6 +6,9 @@ package org.mariadb.jdbc.message.client;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.mariadb.jdbc.client.Context;
 import org.mariadb.jdbc.client.socket.Writer;
 import org.mariadb.jdbc.client.util.Parameter;
@@ -23,8 +26,8 @@ public final class QueryWithParametersPacket implements RedoableClientMessage {
   private final String preSqlCmd;
   private final ClientParser parser;
   private final InputStream localInfileInputStream;
-  private Parameters parameters;
-
+  private List<Parameters> parametersList;
+  
   /**
    * Constructor
    *
@@ -40,24 +43,49 @@ public final class QueryWithParametersPacket implements RedoableClientMessage {
       InputStream localInfileInputStream) {
     this.preSqlCmd = preSqlCmd;
     this.parser = parser;
-    this.parameters = parameters;
+    this.parametersList = List.of(parameters);
     this.localInfileInputStream = localInfileInputStream;
+  }
+  
+  /**
+   * Constructor for a packet containing multiple sets of parameters
+   *
+   * @param preSqlCmd additional pre command
+   * @param parser command parser result
+   * @param parametersList list of parameters
+   */
+  public QueryWithParametersPacket(
+      String preSqlCmd,
+      ClientParser parser,
+      List<Parameters> parametersList) {
+    this.preSqlCmd = preSqlCmd;
+    this.parser = parser;
+    this.parametersList = parametersList;
+    this.localInfileInputStream = null;
   }
 
   @Override
   public void ensureReplayable(Context context) throws IOException, SQLException {
-    int parameterCount = parameters.size();
-    for (int i = 0; i < parameterCount; i++) {
-      Parameter p = parameters.get(i);
-      if (!p.isNull() && p.canEncodeLongData()) {
-        this.parameters.set(
-            i, new org.mariadb.jdbc.codec.Parameter<>(ByteArrayCodec.INSTANCE, p.encodeData()));
+    for (int j = 0; j < parametersList.size(); j++) {
+      Parameters parameters = parametersList.get(j);
+      int parameterCount = parameters.size();
+      for (int i = 0; i < parameterCount; i++) {
+        Parameter p = parameters.get(i);
+        if (!p.isNull() && p.canEncodeLongData()) {
+          parameters.set(
+              i, new org.mariadb.jdbc.codec.Parameter<>(ByteArrayCodec.INSTANCE, p.encodeData()));
+        }
       }
     }
   }
 
+  @Override
   public void saveParameters() {
-    this.parameters = this.parameters.clone();
+    List<Parameters> clonedParameterList = new ArrayList<Parameters>(parametersList.size());
+	for (int j = 0; j < parametersList.size(); j++) {
+	  clonedParameterList.add(parametersList.get(j).clone());
+	}
+	this.parametersList = clonedParameterList;
   }
 
   @Override
@@ -67,7 +95,9 @@ public final class QueryWithParametersPacket implements RedoableClientMessage {
     if (preSqlCmd != null) encoder.writeAscii(preSqlCmd);
     if (parser.getParamPositions().size() == 0) {
       encoder.writeBytes(parser.getQuery());
-    } else {
+      
+    } else if (parser.getValuesBracketPositions() == null || parametersList.size() == 1) {
+      Parameters parameters = parametersList.get(0);
       int pos = 0;
       int paramPos;
       for (int i = 0; i < parser.getParamPositions().size(); i++) {
@@ -77,19 +107,47 @@ public final class QueryWithParametersPacket implements RedoableClientMessage {
         parameters.get(i).encodeText(encoder, context);
       }
       encoder.writeBytes(parser.getQuery(), pos, parser.getQuery().length - pos);
+      
+    } else {
+      // do the rewriting here
+      int startValuePos = parser.getValuesBracketPositions().get(0);
+      int endValuePos = parser.getValuesBracketPositions().get(1);
+
+      // all parameters must be inside the values block.
+      int pos = 0;  // current byte position within parser.getQuery()
+      int paramPos; // next placeholder byte position
+      for (int j = 0; j < parametersList.size(); j++) {
+    	Parameters parameters = parametersList.get(j);
+        for (int i = 0; i < parser.getParamPositions().size(); i++) {
+          paramPos = parser.getParamPositions().get(i);
+          encoder.writeBytes(parser.getQuery(), pos, paramPos - pos);
+          pos = paramPos + 1;
+          parameters.get(i).encodeText(encoder, context);
+        }
+        if (j < parametersList.size() - 1) {
+          encoder.writeBytes(parser.getQuery(), pos, endValuePos - pos + 1);
+          encoder.writeByte(',');
+          pos = startValuePos;
+        }
+      }
+      encoder.writeBytes(parser.getQuery(), pos, parser.getQuery().length - pos);
     }
+    
     encoder.flush();
     return 1;
   }
-
+  
+  @Override
   public int batchUpdateLength() {
     return 1;
   }
 
+  @Override
   public boolean validateLocalFileName(String fileName, Context context) {
-    return ClientMessage.validateLocalFileName(parser.getSql(), parameters, fileName, context);
+    return ClientMessage.validateLocalFileName(parser.getSql(), parametersList.get(0), fileName, context);
   }
 
+  @Override
   public InputStream getLocalInfileInputStream() {
     return localInfileInputStream;
   }
@@ -98,4 +156,5 @@ public final class QueryWithParametersPacket implements RedoableClientMessage {
   public String description() {
     return parser.getSql();
   }
+
 }
