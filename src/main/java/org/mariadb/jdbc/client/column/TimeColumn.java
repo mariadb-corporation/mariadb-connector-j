0ede8eda2f96a95e41942b2004a19bca624acb79
kolzeeq <kolzeeq@gmail.com>
2026-06-05 18:15:00 +0200

    [misc] update license header to 2026

--- src/main/java/org/mariadb/jdbc/client/column/TimeColumn.java
+++ src/main/java/org/mariadb/jdbc/client/column/TimeColumn.java
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 // Copyright (c) 2012-2014 Monty Program Ab
-// Copyright (c) 2015-2024 MariaDB Corporation Ab
+// Copyright (c) 2015-2026 MariaDB Corporation Ab
 package org.mariadb.jdbc.client.column;
 
 import java.sql.*;
 import java.util.Calendar;
-import java.util.Locale;
 import org.mariadb.jdbc.Configuration;
 import org.mariadb.jdbc.client.ColumnDecoder;
 import org.mariadb.jdbc.client.DataType;
