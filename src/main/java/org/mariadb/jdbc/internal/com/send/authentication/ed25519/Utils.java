7a9f6ecf2c9cedb8f9fda1e03355dd987335dcc3
rusher <diego.dupin@mariadb.com>
2026-09-04 17:41:12 +0200

    [misc] ed25519 javadoc correction

--- src/main/java/org/mariadb/jdbc/internal/com/send/authentication/ed25519/Utils.java
+++ src/main/java/org/mariadb/jdbc/internal/com/send/authentication/ed25519/Utils.java
@@ -1,10 +1,10 @@
-/**
+/*
  * EdDSA-Java by str4d
  *
- * <p>To the extent possible under law, the person who associated CC0 with EdDSA-Java has waived all
+ * To the extent possible under law, the person who associated CC0 with EdDSA-Java has waived all
  * copyright and related or neighboring rights to EdDSA-Java.
  *
- * <p>You should have received a copy of the CC0 legalcode along with this work. If not, see
+ * You should have received a copy of the CC0 legalcode along with this work. If not, see
  * <https://creativecommons.org/publicdomain/zero/1.0/>.
  */
 package org.mariadb.jdbc.internal.com.send.authentication.ed25519;
